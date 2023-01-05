f8994454708faa7408cb1a3447d961b334845533
HAMarat <smile56680@gmail.com>
2023-01-05 12:14:16 +0300

    first commit

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-# app.py
-
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+api = Api(app)
+
+movie_ns = api.namespace('movies')
 
 
 class Movie(db.Model):
@@ -18,11 +21,12 @@ class Movie(db.Model):
     description = db.Column(db.String(255))
     trailer = db.Column(db.String(255))
     year = db.Column(db.Integer)
-    rating = db.Column(db.Float)
+    rating = db.Column(db.Integer)
     genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
     genre = db.relationship("Genre")
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
+
 
 class Director(db.Model):
     __tablename__ = 'director'
@@ -35,6 +39,27 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Int()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+
+@movie_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        movies = db.session.query(Movie).all()
+        return movies_schema.dumps(movies, ensure_ascii=False), 200
 
 
 if __name__ == '__main__':
