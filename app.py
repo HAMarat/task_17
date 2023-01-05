775ca24afbd709187115ec96e06ab6c86f6eb360
HAMarat <smile56680@gmail.com>
2023-01-05 23:12:15 +0300

    first commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sqlalchemy
 from flask import Flask, request, jsonify
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +8,8 @@ from marshmallow import Schema, fields
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
+app.url_map.strict_slashes = False
 db = SQLAlchemy(app)
 
 api = Api(app)
@@ -58,9 +61,70 @@ movies_schema = MovieSchema(many=True)
 @movie_ns.route('/')
 class MoviesView(Resource):
     def get(self):
+        # обработка запроса по поиску фильмов по режиссеру
+        did = request.args.get('director_id')
+        if did:
+            movies = db.session.query(Movie).filter(Movie.director_id == did).all()
+            if movies:
+                return movies_schema.dump(movies), 200
+            return "Фильмы такого режиссера не найдены"
+
+        # обработка запроса по поиску фильмов по жанру
+        gid = request.args.get('genre_id')
+        if gid:
+            movies = db.session.query(Movie).filter(Movie.genre_id == gid).all()
+            if movies:
+                return movies_schema.dump(movies), 200
+            return "Фильмы такого жанра не найдены"
+
+        # возвращаем всех пользователей при обращении к адресу '/movies'
         movies = db.session.query(Movie).all()
-        return movies_schema.dumps(movies, ensure_ascii=False), 200
+        if movies:
+            return movies_schema.dump(movies), 200
+        return "Фильм не найден"
+
+    def post(self):
+        movie = request.json
+        # Обработка исключения при попытке добавления записи с не уникальным primary key
+        try:
+            db.session.add(Movie(**movie))
+            db.session.commit()
+            return 'Фильм добавлен', 201
+        except sqlalchemy.exc.IntegrityError as error:
+            return f'{error}'
+
+
+@movie_ns.route('/<int:mid>')
+class MovieView(Resource):
+    def get(self, mid):
+        movie = db.session.query(Movie).get(mid)
+        if movie:
+            return movie_schema.dump(movie), 200
+        return "Фильм не найден", 404
+
+    def put(self, mid):
+        movie = db.session.query(Movie).get(mid)
+        if movie:
+            try:
+                db.session.query(Movie).filter(Movie.id == mid).update(request.json)
+                db.session.commit()
+                return 200
+                # Обработка исключения при попытке обновления записи с не уникальным primary key
+            except sqlalchemy.exc.IntegrityError as error:
+                return f'{error}'
+                # Обработка исключения при попытке обновления записи с не несоответствующими столбцами
+            except sqlalchemy.exc.InvalidRequestError as error:
+                return f'{error}'
+        return f"Фильм с id {mid} не найден"
+
+    def delete(self, mid):
+        movie = db.session.query(Movie).get(mid)
+        if movie:
+            db.session.delete(movie)
+            db.session.commit()
+            return 200
+        return f"Фильм с id {mid} не найден"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
